5cbd8ea08d17b28040a8df4a4c4d1956b55d2b3c
alex cesar <thealexcesar@gmail.com>
2024-09-09 15:33:11 -0300

    structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {ThemeService} from "./services/theme.service";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FaIconComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+  title = 'Car Price Compare'; // TODO: i18n
+  constructor(library: FaIconLibrary, private themeService: ThemeService) {
+    library.addIconPacks(fas);
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+}
---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ThemeService {
+    private readonly themeKey: string = 'data-theme';
+
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+        if (isPlatformBrowser(this.platformId)) {
+            this.setThemeFromLocalStorage();
+        }
+    }
+
+    toggleTheme(): void {
+        if (isPlatformBrowser(this.platformId)) {
+            const currentTheme: string = document.documentElement.getAttribute(this.themeKey) || 'light';
+            const newTheme: string = currentTheme === 'light' ? 'dark' : 'light';
+            document.documentElement.setAttribute(this.themeKey, newTheme);
+            localStorage.setItem(this.themeKey, newTheme);
+        }
+    }
+
+    setThemeFromLocalStorage(): void {
+        if (isPlatformBrowser(this.platformId)) {
+            const storedTheme: string = localStorage.getItem(this.themeKey) || 'light';
+            document.documentElement.setAttribute(this.themeKey, storedTheme);
+
+            if (!localStorage.getItem(this.themeKey)) {
+                const prefersDark: boolean = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                const defaultTheme: string = prefersDark ? 'dark' : 'light';
+                document.documentElement.setAttribute(this.themeKey, defaultTheme);
+                localStorage.setItem(this.themeKey, defaultTheme);
+            }
+        }
+    }
+}
