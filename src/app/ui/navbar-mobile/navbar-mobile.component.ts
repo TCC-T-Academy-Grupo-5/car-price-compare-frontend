d548d5dc2a2b887fc6a6da0e324ee045d10bf8d4
alex cesar <thealexcesar@gmail.com>
2024-10-08 21:11:02 -0300

    🔨 refactor: header using menu to responsive app -> mobile first

---
+++ src/app/ui/navbar-mobile/navbar-mobile.component.ts
@@ -0,0 +1,37 @@
+import {Component, HostListener} from '@angular/core';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {RouterLink} from "@angular/router";
+import {MenuService} from "@services/menu.service";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'tcc-navbar-mobile',
+  standalone: true,
+  imports: [
+    FaIconComponent,
+    RouterLink
+  ],
+  templateUrl: './navbar-mobile.component.html',
+  styles: ``
+})
+export class NavbarMobileComponent {
+  isMenuOpen: any;
+
+  constructor(private menu: MenuService) {}
+
+  toggleMenu(): void {
+    this.menu.toggleMenu();
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+
+    if (!target.closest('.navbar') && this.menu.isMenuOpen) {
+      this.menu.closeMenu();
+    }
+  }
+
+  protected readonly faBars = faBars;
+}
