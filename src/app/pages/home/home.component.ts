5be494e79ea4c3f2a58151b0abb2509c6aa7dc3d
alex cesar <thealexcesar@gmail.com>
2024-10-13 18:08:59 -0300

    mobile menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {fas} from "@fortawesome/free-solid-svg-icons";
-import {NgOptimizedImage} from "@angular/common";
+import {DatePipe, NgOptimizedImage} from "@angular/common";
 import {MatCard, MatCardContent, MatCardImage} from "@angular/material/card";
 import {TranslateModule} from "@ngx-translate/core";
+import {MatTab, MatTabContent, MatTabGroup} from "@angular/material/tabs";
 
 @Component({
   selector: 'tcc-home',
@@ -15,12 +16,23 @@ import {TranslateModule} from "@ngx-translate/core";
     MatCardImage,
     MatCard,
     MatCardContent,
-    TranslateModule
+    TranslateModule,
+    MatTabGroup,
+    MatTab,
+    MatTabContent,
+    DatePipe
   ],
   templateUrl: './home.component.html',
   styles: ``
 })
 export class HomeComponent {
-  constructor() {
+  tabLoadTimes: Date[] = [];
+
+  getTimeLoaded(index: number) {
+    if (!this.tabLoadTimes[index]) {
+      this.tabLoadTimes[index] = new Date();
+    }
+
+    return this.tabLoadTimes[index];
   }
 }
--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,13 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NavbarComponent} from "@ui/navbar/navbar.component";
 import {NavbarMobileComponent} from "@ui/navbar-mobile/navbar-mobile.component";
 import {RouterLink} from "@angular/router";
-import {NgClass} from "@angular/common";
+import {DatePipe, NgClass} from "@angular/common";
 import {MatIconButton} from "@angular/material/button";
 import {MatMenuTrigger} from "@angular/material/menu";
 import {LanguageComponent} from "@ui/language/language.component";
 import {ThemeComponent} from "@ui/theme/theme.component";
 import {UserComponent} from "@ui/user/user.component";
+import {MatTab, MatTabContent, MatTabGroup} from "@angular/material/tabs";
+import {HomeComponent} from "@pages/home/home.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'tcc-header',
@@ -21,18 +25,28 @@ import {UserComponent} from "@ui/user/user.component";
     NgClass,
     RouterLink,
     ThemeComponent,
-    UserComponent
+    UserComponent,
+    MatTabGroup,
+    MatTab,
+    MatTabContent,
+    DatePipe,
+    HomeComponent,
+    MatToolbar,
+    MatIcon
   ],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  // get userMenu(): MatMenuPanel | null {
-  //   return this._userMenu;
-  // }
+  isOpen = false;
 
-  // set userMenu(value: MatMenuPanel | null) {
-  //   this._userMenu = value;
-  // }
-  // title: string = 'precificar';
-  // protected _userMenu: MatMenuPanel | null;
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @ViewChild('section2') section2!: ElementRef;
+  @ViewChild('section3') section3!: ElementRef;
+
+  scrollToSection(section: ElementRef) {
+    section.nativeElement.scrollIntoView({behavior: 'smooth', block: 'start'});
+  }
 }
