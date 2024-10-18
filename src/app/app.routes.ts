5c05bb5b194efe1c211650fc7bbfd955bab68d35
alex cesar <thealexcesar@gmail.com>
2024-10-18 17:00:38 -0300

    container responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from '@pages/home/home.component';
+import {VehiclesComponent} from '@pages/vehicles/vehicles.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent, children: [
-      // { path: 'vehicles', component: VehicleComponent }
-    ]}
+  {path: 'home', component: HomeComponent},
+  {path: 'vehicles', component: VehiclesComponent}
 ];
