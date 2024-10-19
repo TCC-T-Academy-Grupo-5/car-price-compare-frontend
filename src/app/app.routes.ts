0cf3f9939de77e03f9185ee8367e36f3d31c5a76
Paulo Henrique <ph.1928@hotmail.com>
2024-10-18 22:18:48 -0300

    feat: route to vehicle details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from '@pages/home/home.component';
 import {VehiclesComponent} from '@pages/vehicles/vehicles.component';
+import {VehicleDetailsComponent} from '@pages/version-details/vehicle-details.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'vehicles', component: VehiclesComponent}
+  {path: 'vehicles', component: VehiclesComponent},
+  {path: 'vehicle-details/:vehicleId', component: VehicleDetailsComponent}
 ];
