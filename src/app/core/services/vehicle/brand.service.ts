2a6d56c23e11673a377f3d0ee00240b9d8434c49
kingabreel <gabrielyury441@gmail.com>
2024-10-22 19:24:18 -0300

    feat: get brands by name

--- src/app/core/services/vehicle/brand.service.ts
+++ src/app/core/services/vehicle/brand.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {AbstractService} from '@services/vehicle/abstract.service';
 import {PaginatedBrand} from '@domain/vehicle/paginate-brand';
 import {HeadersService} from '@services/headers.service';
+import { Brand } from '@domain/vehicle/brand';
 
 @Injectable({
   providedIn: 'root'
@@ -35,4 +36,17 @@ export class BrandService extends AbstractService<PaginatedBrand, { vehicleType:
     });
   }
 
+  public getByBrand(brand: string): Observable<Brand[]> {
+    return new Observable<Brand[]>((observer) => {
+      this.http.get<Brand[]>(`${this.apiUrl}/${this.apiEndpoint()}?name=${brand}`, { observe: 'response' }).subscribe({
+        next: (response: HttpResponse<Brand[]>) => {
+          observer.next(response.body!);
+          observer.complete();
+        },
+        error: (err) => {
+          observer.error(err);
+        }
+      })
+    });
+  }
 }
