e6244a9b1ad2a72a97f10dcce74330374c37acb8
kingabreel <gabrielyury441@gmail.com>
2024-10-23 22:17:40 -0300

    feat: login functionality returning token

--- src/app/ui/login/login.component.ts
+++ src/app/ui/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Login } from '@domain/user/login';
+import { AuthService } from '@services/auth.service';
+import { Token } from '@domain/user/token';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tcc-login',
@@ -15,7 +19,10 @@ export class LoginComponent {
   loginForm: FormGroup;
   isPopupOpen: boolean = true;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
@@ -28,8 +35,18 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      const loginData = this.loginForm.value;
-      console.log(loginData);
-      this.closePopup();
+      const loginData: Login = this.loginForm.value;
+
+      this.authService.login(loginData).subscribe({
+        next: (response: Token) => {
+          this.snackBar.open('Login successful', 'Close', { duration: 3000 });
+          this.closePopup();
+          localStorage.setItem('token', response.token);
+        },
+        error: (err) => {
+          this.snackBar.open('Login failed: ' + (err.error?.message || 'Unknown error'), 'Close', { duration: 3000 });
+        }
+      });
     }
-  }}
+  }
+}
