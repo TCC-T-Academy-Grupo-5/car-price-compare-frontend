3c4e4a4683dc7f8f47c0b00c3199ea2c5b74e301
Paulo Henrique <ph.1928@hotmail.com>
2024-10-24 10:55:53 -0300

    feat: create NotificationRequest model

--- src/app/core/services/user/notification.service.ts
+++ src/app/core/services/user/notification.service.ts
@@ -2,21 +2,21 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {map} from "rxjs/operators";
+import {NotificationRequest} from '@domain/vehicle/notification-request';
+import {environment} from "@environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
-  private userNotifications: string[] = [];
+  private apiUrl = `${environment.apiUrl}/user/notifications`;
   private notificationsSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
   constructor(private http: HttpClient) {
   }
 
-  createNotification(vehicleId: string): void {
-    console.log('TODO: send POST request to the backend to create new notification, vehicleId: ', vehicleId)
-    this.userNotifications = [...this.userNotifications, vehicleId];
-    this.notificationsSubject.next(this.userNotifications);
+  createNotification(notificationRequest: NotificationRequest): Observable<any> {
+    return this.http.post(this.apiUrl, notificationRequest);
   }
 
   getNotificationsObservable(): Observable<string[]> {
--- src/app/pages/vehicle-details/vehicle-details.component.ts
+++ src/app/pages/vehicle-details/vehicle-details.component.ts
@@ -12,6 +12,7 @@ import {PriceHistoryTableComponent} from '@ui/vehicle/price-history-table/price-
 import {DealsComponent} from '@ui/vehicle/deals/deals.component';
 import {TranslateModule} from '@ngx-translate/core';
 import {NotificationService} from '@services/user/notification.service';
+import {NotificationRequest} from '@domain/vehicle/notification-request';
 
 @Component({
   selector: 'tcc-vehicle-details',
@@ -31,32 +32,40 @@ export class VehicleDetailsComponent implements OnInit, OnDestroy {
   constructor(private vehicleDetailsService: VehicleDetailsService,
               private notificationService: NotificationService,
               private activatedRoute: ActivatedRoute,
-              private errorService: ErrorService) {}
+              private errorService: ErrorService) {
+  }
 
   ngOnInit() {
-     this.vehicleId = this.activatedRoute.snapshot.params['vehicleId'];
-     this.vehicleDetailsSubscription = this.vehicleDetailsService.getVehicleById(this.vehicleId).subscribe({
-       next: (vehicleDetails: VehicleDetails) => {
-         this.vehicleDetails = vehicleDetails;
-         this.latestFipePrice = vehicleDetails.fipePrices.at(0);
-       },
-       error: (error: HttpErrorResponse) => {
-         this.errorService.handleError(error);
-         console.error(error.message);
-       }
-     });
+    this.vehicleId = this.activatedRoute.snapshot.params['vehicleId'];
+    this.vehicleDetailsSubscription = this.vehicleDetailsService.getVehicleById(this.vehicleId).subscribe({
+      next: (vehicleDetails: VehicleDetails) => {
+        this.vehicleDetails = vehicleDetails;
+        this.latestFipePrice = vehicleDetails.fipePrices.at(0);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.errorService.handleError(error);
+        console.error(error.message);
+      }
+    });
 
-     this.disableNewAlertSubscription = this.notificationService.isUserSubscribedToVehicle(this.vehicleId).subscribe({
-       next: (isAlreadySubscribed: boolean) => this.shouldDisableAlerts = isAlreadySubscribed,
-     })
+    this.disableNewAlertSubscription = this.notificationService.isUserSubscribedToVehicle(this.vehicleId).subscribe({
+      next: (isAlreadySubscribed: boolean) => this.shouldDisableAlerts = isAlreadySubscribed,
+    })
   }
 
   ngOnDestroy() {
-      this.vehicleDetailsSubscription?.unsubscribe();
-      this.disableNewAlertSubscription?.unsubscribe();
+    this.vehicleDetailsSubscription?.unsubscribe();
+    this.disableNewAlertSubscription?.unsubscribe();
   }
 
   onAlertClick() {
-    this.notificationService.createNotification(this.vehicleId);
+    const notificationRequest: NotificationRequest = {
+      notificationType: 0,
+      currentFipePrice: this.latestFipePrice?.price ?? 0,
+      vehicleId: this.vehicleId
+    };
+    this.notificationService.createNotification(notificationRequest).subscribe({
+      next: response => console.log("Notificação criada: " + response),
+    });
   }
 }
