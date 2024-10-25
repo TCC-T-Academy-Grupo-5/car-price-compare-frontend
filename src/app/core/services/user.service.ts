39424afd64a2948c706a26d0b98378ddb5ac8018
kingabreel <gabrielyury441@gmail.com>
2024-10-25 19:39:57 -0300

    service to manipulate user data

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Update } from '@domain/user/update';
+import { environment } from '@environments/environment.development';
+import { catchError, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = `${environment.apiUrl}/user`;
+
+  constructor(private http: HttpClient) {}
+  
+  public update(data: Update, id: string | undefined): Observable<Update> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  
+    return this.http.put<Update>(`${this.apiUrl}/${id}`, data, { headers }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    return new Observable<never>((observer) => {
+      observer.error(error);
+    });
+  }
+}
