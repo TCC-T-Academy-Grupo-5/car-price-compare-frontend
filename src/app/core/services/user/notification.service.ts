2e3be4e8209fb5e5ab043679873f0bee641fde16
kingabreel <gabrielyury441@gmail.com>
2024-10-28 20:12:45 -0300

    feat: notification service updated

--- src/app/core/services/user/notification.service.ts
+++ src/app/core/services/user/notification.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {NotificationRequest} from '@domain/vehicle/notification-request';
 import {environment} from "@environments/environment";
@@ -13,6 +13,13 @@ export class NotificationService {
     private notificationsSubject: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
 
     constructor(private http: HttpClient) {
+    }
+
+    getNotification(): Observable<NotificationResponse[]> {
+        const token = localStorage.getItem('token');
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+        return this.http.get<NotificationResponse[]>(this.apiUrl,  { headers } );
     }
 
     createNotification(notificationRequest: NotificationRequest): Observable<NotificationResponse> {
