971a31bb9f5b61e6468a80a7215703844a5cf8fe
Paulo Henrique Meneses <124916478+paulohenrique-gh@users.noreply.github.com>
2024-10-30 18:05:05 -0300

    Componente para filtrar veículos (#38)
    * wip: implementing options
    
    * feat: cascading filters
    
    * feat: form now redirects to vehicle details page when version is selected
    
    * wip: styling
    
    * chore: remove duplicated component
    
    * fix: filter fields now reset properly
    
    * style: layout adjustment
    
    * chore: move filter component to new ui folder
    
    * feat: add translations to filter
    
    * refactor: change button color when disabled
    
    * refactor: search bar extracted to search component
    
    * fix: favorite color on dark mode
    
    * fix: remove unused variable
    
    * fix: remove unused methods
    
    * fix: translation keys
    
    * chore: fix lint issues

---
+++ src/app/components/ui/filter/filter.component.ts
@@ -0,0 +1,112 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {HttpErrorResponse} from '@angular/common/http';
+import {ErrorService} from '@services/errors/error.service';
+import {SelectOption} from '@domain/vehicle/select-option';
+import {OptionService} from '@services/vehicle/option.service';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'tcc-filter',
+  standalone: true,
+  imports: [CommonModule, TranslateModule, FormsModule],
+  templateUrl: './filter.component.html',
+  host: {class: 'w-full md:w-3/5 mb-10'}
+})
+export class FilterComponent {
+  @ViewChild('typeSelect') typeSelect!: ElementRef<HTMLSelectElement>;
+  @ViewChild('brandSelect') brandSelect!: ElementRef<HTMLSelectElement>;
+  @ViewChild('modelSelect') modelSelect!: ElementRef<HTMLSelectElement>;
+  @ViewChild('yearSelect') yearSelect!: ElementRef<HTMLSelectElement>;
+  @ViewChild('vehicleSelect') vehicleSelect!: ElementRef<HTMLSelectElement>;
+
+  vehicleTypes = {
+    'car': 0,
+    'motorcycle': 1,
+    'truck': 2
+  }
+  brandOptions: SelectOption[] = [];
+  modelOptions: SelectOption[] = [];
+  yearOptions: SelectOption[] = [];
+  vehicleOptions: SelectOption[] = [];
+
+  selectedType: number | undefined;
+  selectedBrandId: string | undefined;
+  selectedModelId: string | undefined;
+  selectedYearId: string | undefined;
+  selectedVehicleId: string | undefined;
+
+  constructor(private optionService: OptionService,
+              private router: Router,
+              private errorService: ErrorService) {}
+
+  onTypeChange($event: Event) {
+    this.selectedType = Number(($event.target as HTMLSelectElement).value);
+
+    this.optionService.findOptions(this.selectedType, 'brand').subscribe({
+      next: (brandOptions: SelectOption[]) => this.brandOptions = brandOptions,
+      error: this.handleError.bind(this)
+    });
+  }
+
+  onBrandChange($event: Event) {
+    this.selectedBrandId = ($event.target as HTMLSelectElement).value;
+
+    this.optionService.findOptions(this.selectedBrandId, 'model').subscribe({
+      next: (modelOptions: SelectOption[]) => this.modelOptions = modelOptions,
+      error: this.handleError.bind(this)
+    });
+  }
+
+  onModelChange($event: Event) {
+    this.selectedModelId = ($event.target as HTMLSelectElement).value;
+
+    this.optionService.findOptions(this.selectedModelId, 'year').subscribe({
+      next: (yearOptions: SelectOption[]) => this.yearOptions = yearOptions,
+      error: this.handleError.bind(this)
+    });
+  }
+
+  onYearChange($event: Event) {
+    this.selectedYearId = ($event.target as HTMLSelectElement).value;
+
+    this.optionService.findOptions(this.selectedYearId, 'vehicle').subscribe({
+      next: (vehicleOptions: SelectOption[]) => this.vehicleOptions = vehicleOptions,
+      error: this.handleError.bind(this)
+    });
+  }
+
+  onVehicleChange($event: Event) {
+    this.selectedVehicleId = ($event.target as HTMLSelectElement).value;
+  }
+
+  onSubmit() {
+    this.router.navigate(['/vehicle-details', this.selectedVehicleId]);
+  }
+
+  onReset() {
+    this.typeSelect.nativeElement.selectedIndex = 0;
+    this.brandSelect.nativeElement.selectedIndex = 0;
+    this.modelSelect.nativeElement.selectedIndex = 0;
+    this.yearSelect.nativeElement.selectedIndex = 0;
+    this.vehicleSelect.nativeElement.selectedIndex = 0;
+
+    this.selectedType = undefined;
+    this.selectedBrandId = undefined;
+    this.selectedModelId = undefined;
+    this.selectedYearId = undefined;
+    this.selectedVehicleId = undefined;
+    this.brandOptions = [];
+    this.modelOptions = [];
+    this.yearOptions = [];
+    this.vehicleOptions = [];
+  }
+
+  private handleError(error: HttpErrorResponse): void {
+    this.errorService.handleError(error);
+    console.error(error.message);
+  }
+}
+
---
+++ src/app/core/services/vehicle/option.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {SelectOption} from '@domain/vehicle/select-option';
+import {environment} from '@environments/environment.development';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OptionService {
+  private entrypoint = `${environment.entrypoint}`;
+
+  private optionsPaths = {
+    brand: (parentId: string | number) => `brand/${parentId}/options`,
+    model: (parentId: string | number) => `model/brand/${parentId}/options`,
+    year: (parentId: string | number) => `year/model/${parentId}/options`,
+    vehicle: (parentId: string | number) => `vehicle/year/${parentId}/options`,
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  public findOptions(parentId: string | number, optionType: 'brand' | 'model' | 'year' | 'vehicle'): Observable<SelectOption[]> {
+    const path = this.optionsPaths[optionType](parentId);
+    return this.http.get<SelectOption[]>(`${this.entrypoint}/${path}`).pipe(
+      map((options: SelectOption[]) => options.sort((a, b) => a.name.localeCompare(b.name)))
+    );
+  }
+}
