54415ef511ff08ce087c5e66c9db5da2f7d35204
alex cesar <thealexcesar@gmail.com>
2024-10-30 22:05:45 -0300

    fix/home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -3,7 +3,7 @@ import { CommonModule, DatePipe, NgOptimizedImage } from '@angular/common';
 import { MatCard, MatCardContent, MatCardImage } from '@angular/material/card';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatTab, MatTabContent, MatTabGroup } from '@angular/material/tabs';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
 import { InteractionDirective } from '@directives/EventListenerDirectives';
 import { FilterTypeComponent } from '@components/ui/vehicle/filter-type/filter-type.component';
 import { FilterBrandComponent } from '@components/ui/vehicle/filter-brand/filter-brand.component';
@@ -14,6 +14,7 @@ import { TranslationsPipe } from '@pipes/translations.pipe';
 import { FilterComponent } from '@components/ui/filter/filter.component';
 import { SpinnerComponent } from '@components/shared/spinner/spinner.component';
 import { SearchComponent } from '@components/ui/search/search.component';
+import {Model} from '@domain/vehicle/model';
 
 @Component({
   selector: 'tcc-home',
@@ -54,7 +55,7 @@ export class HomeComponent implements OnInit {
   vehicleImgMobile!: string;
   showLoadMore = true;
 
-  constructor(private brandService: BrandService) {}
+  constructor(private brandService: BrandService, private router: Router) {}
 
   ngOnInit(): void {
     this.selectedType = Number(localStorage.getItem('vehicleType')) || this.selectedType;
@@ -90,7 +91,8 @@ export class HomeComponent implements OnInit {
           this.brands = response;
           this.showLoadMore = true;
         }
-      }, error: (error) => console.error("Erro ao carregar marcas:", error.message)
+      },
+      error: (error) => console.error('Erro ao carregar marcas:', error.message),
     });
   }
 
@@ -115,41 +117,33 @@ export class HomeComponent implements OnInit {
   }
 
   onSearchTextChange(): void {
-
     localStorage.setItem('searchText', this.searchText);
     if (this.searchText.length > 0) {
-      this.modelService.getByModel(this.searchText).subscribe({
-        next: (response) => {
-          this.modelSuggestions = response;
-        }, error: (err) => console.error('Erro ao carregar modelos:', err.message)
-      });
-
       this.brandService.getByBrand(this.searchText).subscribe({
-        next: (response) =>  this.brandsSuggestions = response,
-        error: (err) => console.error('Erro ao carregar marcas:', err.message)
+        next: (response) => (this.brands = response),
+        error: (err) => console.error('Erro ao carregar marcas:', err.message),
       });
     } else {
-      this.modelSuggestions = [];
-      this.brandsSuggestions = [];
+      this.getPopularBrands();
     }
   }
 
   onBrandSelected(name: string | undefined): void {
     if (name) {
-      this.router.navigate(['/models'], {state: {brand: name, vehicleType: this.selectedType}}).then();
-     }
+      this.router.navigate(['/models'], { state: { brand: name, vehicleType: this.selectedType } }).then();
+    }
   }
 
   onModelSelected(model: Model): void {
     if (model.name) {
-      const selectedBrand = this.brands.find(b => b.id === model.brandId);
+      const selectedBrand = this.brands.find((b) => b.id === model.brandId);
       if (selectedBrand) {
         this.router.navigate(['/vehicles'], {
           queryParams: {
             model: model.name,
             imageUrl: model.imageUrl || '',
-            brand: selectedBrand.name
-          }
+            brand: selectedBrand.name,
+          },
         }).then();
       }
     }
